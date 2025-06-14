6ed116bb844860771ee3993a665dc87174cbde37
ChonicDrop <amammadli007@gmail.com>
2025-06-14 18:16:01 +0400

    Cheese for Anfisa - 2

---
+++ EOLYMP/1-100/67.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll a, b, c, count = 0; cin>>a>>b>>c;
+    ll h = a*b*c;
+    cout<<h-1;
+    return 0;
+}
