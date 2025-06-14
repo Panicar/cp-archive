33f8db970e314d569de849019f4661e460b7e883
ChonicDrop <amammadli007@gmail.com>
2025-06-14 18:20:49 +0400

    Square

---
+++ EOLYMP/101-200/124.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n;
+
+void solve(){
+    while (cin >> n)
+    {
+        long long p = n * 4, s = n * n;
+
+        cout << p << " " << s << endl;
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
